e0cb1c1e14dc6de261875bdb37637b3053714b9a
IceKspRt <152171925+IceKspRt@users.noreply.github.com>
2023-12-21 18:46:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
+<!DOCTYPE html>
 <html>
-    <body>
-        <?php> echo 'Hello World'; ?>
-        adsadsad
-    </body>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+    
+    <?php
+        isset( $_POST['base'] ) ? $base = $_POST['base'] : $base = "";
+        if( !empty( $base ) ) {
+            echo "<hr/>";
+            echo "<b>สูตรคูณแม่ {$base}</b><br/><br/>";
+            
+        }
+        for( $i=1; $i<=24; $i++ ) {
+            echo "2 x {$i} = ".(2*$i)."<br/>";
+        }
+    ?>
+</body>
 </html>

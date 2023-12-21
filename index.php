b4ab818f057804eeed9b9141ae96e51bb1e845fb
IceKspRt <152171925+IceKspRt@users.noreply.github.com>
2023-12-21 17:40:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-<html>
+x<html>
     <body>
+<<<<<<< Updated upstream
         <?php> echo 'Hello World' ; ?>
+=======
+        <h1>This is PHP <?php> echo 'Hello Word' ; ?></h1>
+        <div>
+            <p>1</p>
+            
+        </div>
+>>>>>>> Stashed changes
     </body>
 </html>

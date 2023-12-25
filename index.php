f6c0b07975121ef6a3e6708aac44b2d2e5b6e9ac
IceKspRt <152171925+IceKspRt@users.noreply.github.com>
2023-12-26 00:01:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,27 @@
 <meta charset="utf-8">
-    <link rel="stylesheet" href="mystyle.css" />
+<link rel="stylesheet" href="mystyle.css" />
 </head>
 
 <body>
     <center>
-    <?php $multi_x = 2; ?>
-    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
-    <div class="scroll">
-    
-    <?php
-        if( !empty($multi_x) ) {
-    for( $i=1; $i<=24; $i++ ) {
-        echo"<p>";
-        echo "$multi_x x {$i} = ".( $multi_x *$i)."<br/>";
-        echo"</p>";
-    }
-}
-?>     
-
-
-          
-</div>
+        <?php $multi_x = 2; ?>
+        <h1>ตารางสูตรคูณแม่
+            <?php echo $multi_x ?>
+        </h1>
+        <div class="scroll">
+            <?php
+            if (!empty($multi_x)) {
+                for ($i = 1; $i <= 24; $i++) {
+                    echo "<p>";
+                    echo "$multi_x x {$i} = " . ($multi_x * $i) . "<br/>";
+                    echo "</p>";
+                }
+            }
+            ?>
+        </div>
 
     </center>
-    
+
 </body>
+
 </html>

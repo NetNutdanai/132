f9b2cb88e4c4bbce906c467c1ee411698d5be3c9
IceKspRt <152171925+IceKspRt@users.noreply.github.com>
2023-12-25 23:54:14 +0700

    25/12/2566

--- index.php
+++ index.php
@@ -1,29 +1,28 @@
 <meta charset="utf-8">
-    <link rel="stylesheet" href="w3.css" />
+    <link rel="stylesheet" href="mystyle.css" />
 </head>
 
 <body>
+    <center>
     <?php $multi_x = 2; ?>
     <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
-    <style>
-        .scroll {
-            width: 150px;
-            height: 265px;
-            border: 1px solid black; 
-            overflow:auto; 
-        }
-    </style>
     <div class="scroll">
-            <tr>
-            <?php
-            if( !empty($multi_x) ) {
-        for( $i=1; $i<=24; $i++ ) {
-            echo"<k>";
-            echo "&nbsp$multi_x x {$i} = ".( $multi_x *$i)."<br/><br/>";
-            echo"</k>";
-        }
+    
+    <?php
+        if( !empty($multi_x) ) {
+    for( $i=1; $i<=24; $i++ ) {
+        echo"<p>";
+        echo "$multi_x x {$i} = ".( $multi_x *$i)."<br/>";
+        echo"</p>";
     }
-    ?>       
-    </div>
+}
+?>     
+
+
+          
+</div>
+
+    </center>
+    
 </body>
 </html>

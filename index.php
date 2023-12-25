8ee19daf3a7b5642e4da85b3d1e65aa74cb368ab
IceKspRt <152171925+IceKspRt@users.noreply.github.com>
2023-12-25 17:44:19 +0700

    new

--- index.php
+++ index.php
@@ -1,15 +1,29 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
+<meta charset="utf-8">
+    <link rel="stylesheet" href="w3.css" />
 </head>
+
 <body>
-    <h4>ตารางสูตรคูณแม่ 2</h4>
-    <?php
-        echo "<hr/>";
-        for( $i=1; $i<=24; $i++ ) {
-            echo "2 x {$i} = ".(2*$i)."<br/>";
+    <?php $multi_x = 2; ?>
+    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+    <style>
+        .scroll {
+            width: 150px;
+            height: 265px;
+            border: 1px solid black; 
+            overflow:auto; 
         }
-    ?>
+    </style>
+    <div class="scroll">
+            <tr>
+            <?php
+            if( !empty($multi_x) ) {
+        for( $i=1; $i<=12; $i++ ) {
+            echo"<p>";
+            echo "$multi_x x {$i} = ".( $multi_x *$i)."<br/>";
+            echo"</p>";
+        }
+    }
+    ?>       
+    </div>
 </body>
 </html>

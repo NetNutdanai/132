cafc33f5ada4b36685e65277512cbe94d6316daf
IceKspRt <152171925+IceKspRt@users.noreply.github.com>
2024-02-06 00:12:56 +0700

    05/02/66_Logout

--- example-app/resources/views/login.blade.php
+++ example-app/resources/views/login.blade.php
@@ -20,12 +20,11 @@
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="../../index2.html"><b>Admin</b>LTE</a>
+            <a href="../../index2.html"><b>SE</b>Camp</a>
         </div>
         <!-- /.login-logo -->
         <div class="card">
             <div class="card-body login-card-body">
-                <p class="login-box-msg">Sign in to start your session</p>
 
                 <form action="{{ url('login') }}" method="post">
                     @csrf
